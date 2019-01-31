e58207ded1537a4f9c4889203ac91dd9a0a40c71
Hiroshi Ueki <hirocueki@gmail.com>
2019-01-31 14:39:00 +0900

    見た目の調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,6 +7,7 @@ class HomesController < ApplicationController
   def code
     message = params[:message]
     if message.present?
+      @hash_tag = URI.decode(message)
       @request_url = "https://twitter.com/intent/tweet?text="+message
     end
   end
